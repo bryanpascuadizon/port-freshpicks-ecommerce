4118684473e51fec999cfe01065801cf85b4f1d5
Bryan Dizon <dizon_danielle@yahoo.com>
2025-04-29 14:59:36 +0800

    header ui improvement

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { SignOut } from "@/lib/actions/UserActions";
 import { APP_NAME } from "@/lib/constants";
 import { AlignJustify, ListOrdered, Power, User } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import ShoppingCartHeader from "./ShoppingCart";
 import siteImage from "@/assets/site-icon.png";
 import { Popover, PopoverContent } from "@radix-ui/react-popover";
@@ -11,7 +13,9 @@ import { PopoverTrigger } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 
-const Header = async () => {
+const Header = () => {
+  const [popoverOpen, setPopOverOpen] = useState(false);
+
   return (
     <header className="w-full border-b-1 border-b-slate-100">
       <div className="wrapper flex-between">
@@ -26,29 +30,40 @@ const Header = async () => {
         <nav className="">
           <div className="flex justify-end gap-10">
             <ShoppingCartHeader />
-            <Popover>
+            <Popover open={popoverOpen} onOpenChange={setPopOverOpen}>
               <PopoverTrigger>
                 <AlignJustify className="cursor-pointer" />
               </PopoverTrigger>
-              <PopoverContent className="">
+              <PopoverContent className="z-50">
                 <div className="flex flex-col rounded-sm bg-white border-1 border-slate-200 mt-3">
                   <div className="p-3 flex flex-col">
-                    <Link href="/user/account/profile" className="mb-1 flex">
-                      <User className="mr-4" />
+                    <Link
+                      href="/user/account/profile"
+                      className="mb-3 flex text-sm"
+                      onClick={() => setPopOverOpen(false)}
+                    >
+                      <User className="mr-4" width={20} height={20} />
                       Profile
                     </Link>
 
-                    <Link href="/user/orders" className="mb-1 flex">
-                      <ListOrdered className="mr-4" />
+                    <Link
+                      href="/user/orders"
+                      className="flex text-sm"
+                      onClick={() => setPopOverOpen(false)}
+                    >
+                      <ListOrdered className="mr-4" width={20} height={20} />
                       My Orders
                     </Link>
                   </div>
                   <Separator />
-                  <Button onClick={SignOut} className="cursor-pointer">
+                  <p
+                    onClick={SignOut}
+                    className="p-3 flex cursor-pointer text-sm"
+                  >
                     {" "}
-                    <Power className="mr-4" />
+                    <Power className="mr-4" width={20} height={20} />
                     Logout
-                  </Button>
+                  </p>
                 </div>
               </PopoverContent>
             </Popover>
