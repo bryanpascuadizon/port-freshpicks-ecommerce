27b7d8fd3a8e822d849309b6c84da97c73593071
Bryan Dizon <dizon_danielle@yahoo.com>
2025-05-06 17:43:44 +0800

    implement initial ui for shipping orders

--- components/shared/order/OrderTabContent.tsx
+++ components/shared/order/OrderTabContent.tsx
@@ -82,32 +82,43 @@ const OrderTabContent = ({
                 </span>
                 <p className="mt-1">{order.shippingAddress.address}</p>
               </div>
-              <div className="flex justify-start md:justify-end gap-2">
-                <Button
-                  className="green-button-alternate min-w-[120px]"
-                  onClick={() => handleCancelOrder(order.referenceNumber)}
-                >
-                  {isCancelPending ? (
-                    <Loader className="animate-spin" />
-                  ) : (
-                    "Cancel"
-                  )}
-                </Button>
-                <Button
-                  className="green-button min-w-[120px]"
-                  onClick={() => handlePaymentOrder(order)}
-                >
-                  {" "}
-                  {isPaymentPending ? (
-                    <Loader className="animate-spin" />
-                  ) : (
-                    "Pay"
-                  )}
-                </Button>
-                <p className="font-bold text-green text-lg self-center">
-                  {currencyFormatter.format(order.subtotalPrice)}
-                </p>
-              </div>
+              {stage === "topay" && (
+                <div className="">
+                  <p className="text-right text-sm mb-5">
+                    Shipping Address:{" "}
+                    <span className="font-bold text-green">
+                      {currencyFormatter.format(order.shippingPrice)}
+                    </span>
+                  </p>
+
+                  <div className="flex justify-end gap-2">
+                    <Button
+                      className="green-button-alternate min-w-[120px] self-center"
+                      onClick={() => handleCancelOrder(order.referenceNumber)}
+                    >
+                      {isCancelPending ? (
+                        <Loader className="animate-spin" />
+                      ) : (
+                        "Cancel"
+                      )}
+                    </Button>
+                    <Button
+                      className="green-button min-w-[120px] self-center"
+                      onClick={() => handlePaymentOrder(order)}
+                    >
+                      {" "}
+                      {isPaymentPending ? (
+                        <Loader className="animate-spin" />
+                      ) : (
+                        "Pay"
+                      )}
+                    </Button>
+                    <p className="font-bold text-green text-lg self-center">
+                      {currencyFormatter.format(order.totalPrice)}
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         ))}
