85f5b10171b0eaa335e8b6e3c9762e7f8a8bef5e
Bryan Dizon <dizon_danielle@yahoo.com>
2025-05-06 11:40:55 +0800

    added rights in footer

--- components/shared/footer/index.tsx
+++ components/shared/footer/index.tsx
@@ -8,17 +8,26 @@ const Footer = () => {
   return (
     <footer className="w-full border-t-1 border-t-slate-200">
       <div className="wrapper flex-between">
-        <div className="flex-start">
+        <nav className="flex-start">
           <Link href="/" className="flex">
             <Image src={siteImage} alt="site-icon" width="50" height="50" />
             <span className="font-bold text-3xl ml-3 text-green self-center">
               {APP_NAME}
             </span>
-            <span className="self-center mr-auto">
-              © 2025 Bryan Dizon | All Rights Reserved
-            </span>
           </Link>
-        </div>
+        </nav>
+        <nav className="flex justify-end">
+          <span className="self-center mr-auto">
+            © 2025{" "}
+            <Link
+              href="https://github.com/bryanpascuadizon"
+              className="text-green"
+            >
+              Bryan Dizon
+            </Link>{" "}
+            | All Rights Reserved
+          </span>
+        </nav>
       </div>
     </footer>
   );
